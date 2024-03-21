46af1c8e27433c8a19e4aff14b594b2da1f48dbd
Matheus Vitor Ferreira <matheusvitor.dev00@gmail.com>
2024-03-21 15:12:35 -0300

    Criação do chatWebSocket

--- src/main/java/com/matheus/chatwebsocket/controller/RoomChatController.java
+++ src/main/java/com/matheus/chatwebsocket/controller/RoomChatController.java
@@ -1,6 +1,13 @@
 package com.matheus.chatwebsocket.controller;
 
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -14,35 +21,43 @@ import org.springframework.web.bind.annotation.RestController;
 import com.matheus.chatwebsocket.model.RoomChat;
 import com.matheus.chatwebsocket.service.RoomService;
 
+import jakarta.annotation.Resource;
+
 @RestController
 @RequestMapping("/chat")
 public class RoomChatController {
 
-    @Autowired
-    private RoomService roomService;
+	@Autowired
+	private RoomService roomService;
 
-    @PostMapping("/message")
-    public ResponseEntity<Object> createRoomAndLogin() {
-        try {
-            RoomChat newRoom = roomService.createRoom();
-            return ResponseEntity.ok("Room created successfully!");
-        } catch (Exception e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
-        }
-    }
+	@GetMapping("/message")
+	public ResponseEntity<Object> enterChatRoom() {
+	    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+	    if (authentication == null || !authentication.isAuthenticated()) {
+	        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+	    }
+	    RoomChat newRoom = roomService.createRoom();
+	    // Ler o conteúdo do arquivo HTML e retorná-lo
+	    try {
+	        ClassPathResource resource = new ClassPathResource("/static/chat.html");
+	        InputStream inputStream = resource.getInputStream();
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+	        StringBuilder stringBuilder = new StringBuilder();
+	        String line;
+	        while ((line = reader.readLine()) != null) {
+	            stringBuilder.append(line).append('\n');
+	        }
+	        return ResponseEntity.ok().body(stringBuilder.toString());
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to load HTML content");
+	    }
+	}
 
-    @GetMapping("/message")
-    public ResponseEntity<Object> enterChatRoom() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || !authentication.isAuthenticated()) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
-        }
-        
-        return ResponseEntity.ok("Welcome to the chat room!");
-    }
 	@PostMapping("/disconnect")
 	public ResponseEntity<String> disconnectUserFromRoom(@RequestParam Long roomId, @RequestParam Long userId) {
 		roomService.removeUserFromRoom(roomId, userId);
 		return ResponseEntity.ok("User disconnected from the room successfully!");
 	}
+
 }
--- src/main/resources/static/chat.js
+++ src/main/resources/static/chat.js
@@ -1,32 +1,9 @@
-async function createChatRoom() {
-    try {
-        const response = await fetch("/chat/message");
-        
-        if (response.ok) {
-            const htmlContent = await response.text();
-            document.body.innerHTML = htmlContent;
-            if (isConnected) { // Se isConnected for true, chama a função connect()
-                connect();
-            }
-        } else {
-            console.error("Failed to load chat room:", response.statusText);
-            alert("Failed to load chat room. Please try again later.");
-        }
-    } catch (error) {
-        console.error("Error loading chat room:", error);
-        alert("An error occurred while loading the chat room. Please try again later.");
-    }
-}
-
 function sendMessage(e) {
     e.preventDefault();
     const messageInput = document.getElementById("messageInput").value;
 
-    const message = {
-        user: sessionStorage.getItem("user"),
-        msg: messageInput
-    };
-
+    displayMessage(messageInput,sessionStorage.getItem('user'))
+   
     Client.send("/app/chatMessage", {}, JSON.stringify(message));
 
     document.getElementById("messageInput").value = "";
@@ -46,20 +23,7 @@ function connect(){
 function displayMessage(message, user) {
     const chatMessages = document.getElementById("chatMessages");
     const messageContainer = document.createElement("div");
-    const messageText = document.createElement("div");
-    const userText = document.createElement("div");
-
-    messageContainer.classList.add("message-container");
-    messageText.textContent = message;
-    userText.textContent = user;
-
-    messageContainer.appendChild(userText);
-    messageContainer.appendChild(messageText);
+   
+    messageContainer.textContent = user + ": " + message;
     chatMessages.appendChild(messageContainer);
 }
-
-document.addEventListener("DOMContentLoaded", function() {
-    if (sessionStorage.getItem("user")) { // Verifica se o usuário está autenticado
-        createChatRoom(); // Chama createChatRoom() apenas se o usuário estiver autenticado
-    }
-});
